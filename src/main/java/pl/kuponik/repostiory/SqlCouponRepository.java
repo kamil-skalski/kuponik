7ca4f480ece883a91c2679df9b4d56afed74be0d
Artur Laskowski <artur.laskowski@comarch.com>
2024-06-24 16:00:47 +0200

    change repo

--- src/main/java/pl/kuponik/repostiory/SqlCouponRepository.java
+++ src/main/java/pl/kuponik/repostiory/SqlCouponRepository.java
@@ -1,30 +1,10 @@
 package pl.kuponik.repostiory;
 
-import lombok.AllArgsConstructor;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import pl.kuponik.model.Coupon;
 
-import java.util.Optional;
 import java.util.UUID;
 
-@Repository
-@AllArgsConstructor
-public class SqlCouponRepository implements CouponRepository {
-
-    private final CouponRepositoryJpa couponRepositoryJpa;
-
-    @Override
-    public Optional<Coupon> findById(UUID id) {
-        return couponRepositoryJpa.findById(id);
-    }
-
-    @Override
-    public Coupon save(Coupon coupon) {
-        return couponRepositoryJpa.save(coupon);
-    }
+public interface SqlCouponRepository extends CouponRepository, JpaRepository<Coupon, UUID> {
 }
 
-@Repository
-interface CouponRepositoryJpa extends CrudRepository<Coupon, UUID> {
-}
